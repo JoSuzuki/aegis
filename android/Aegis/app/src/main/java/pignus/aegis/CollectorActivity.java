84631f888cdc1147a1ce720d49cb3aa6e6961042
JoSuzuki <jonathan@suzuki.pro.br>
2018-08-26 21:37:04 -0300

    Polish user interface

---
+++ android/Aegis/app/src/main/java/pignus/aegis/CollectorActivity.java
@@ -0,0 +1,250 @@
+package pignus.aegis;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class CollectorActivity extends AppCompatActivity implements SensorEventListener{
+    private final static int STRING_MAX_SIZE = 10000;
+    File folder;
+
+    File AccelerometerFile;
+    File GyroscopeFile;
+    File MagnetometerFile;
+    File TouchEventFile;
+
+
+    String BufferAccel = "";
+    String BufferGyro = "";
+    String BufferMag = "";
+    String BufferTouch = "";
+
+    CollectorKeyboard mCollectorKeyboard;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer, mGyroscope, mMagnetometer;
+    Display display;
+    int PhoneOrientation;
+    int SensorType;
+    int eventAction;
+
+    //Valor dos Sensores
+    String AccelX, AccelY, AccelZ;
+    String GyroX, GyroY, GyroZ;
+    String MagX, MagY, MagZ;
+    String unixTime, PosX, PosY, Press, Area;
+
+    //Txt da Tela
+    EditText textBox;
+    TextView mTxtUserName;
+
+    private String GravarArquivo(File Arquivo, String bufferDados, String dados, boolean Buffer){
+        if(bufferDados.length() + dados.length() > STRING_MAX_SIZE || Buffer == false) {
+            try {
+                FileOutputStream fOut = new FileOutputStream(Arquivo, true);
+                OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+                myOutWriter.write(bufferDados + dados);
+                myOutWriter.flush();
+                myOutWriter.close();
+                fOut.close();
+                bufferDados = "";
+                Log.i("Diego", "File Writen");
+            } catch (Exception e) {
+                Log.e("Diego", "Could not write on file");
+            }
+        }else {
+            bufferDados = bufferDados + dados;
+            Log.i("Diego", Integer.toString(bufferDados.length() + dados.length()));
+        }
+        return bufferDados;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        setContentView(R.layout.activity_collector);
+        String userName = intent.getStringExtra(MainActivity.USER_NAME);
+
+        String Folder = userName;
+        folder = new File(Environment.getExternalStorageDirectory() + File.separator + Folder);
+
+        AccelerometerFile = new File("/sdcard/" + Folder + "/Accelerometer.csv");
+        GyroscopeFile = new File("/sdcard/" + Folder + "/Gyroscope.csv");
+        MagnetometerFile = new File("/sdcard/" + Folder + "/Magnetometer.csv");
+        TouchEventFile = new File("/sdcard/" + Folder + "/TouchEvent.csv");
+
+        //Txtbox editavel da tela
+        textBox = (EditText) findViewById(R.id.editText);
+
+        // Collector keyboard
+        mCollectorKeyboard = new CollectorKeyboard(this, R.id.keyboardview, R.xml.collector_keyboard, userName);
+        mCollectorKeyboard.registerEditText(R.id.editText);
+
+        //Txts da Tela
+
+        mTxtUserName = (TextView) findViewById(R.id.TxtUserName);
+        mTxtUserName.setText(userName);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mMagnetometer  = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        mSensorManager.registerListener(this, mAccelerometer, 100000000);
+        mSensorManager.registerListener(this, mGyroscope, 100000000);
+        mSensorManager.registerListener(this, mMagnetometer, 100000000);
+
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        if(!AccelerometerFile.exists()){
+            try {
+                AccelerometerFile.createNewFile();
+                Log.i("Diego", "AccelerometerFile Created");
+            } catch (Exception e) {
+                Log.e("Diego", "Could not create AccelerometerFile",e);
+            }
+        }
+
+        if(!GyroscopeFile.exists()){
+            try {
+                GyroscopeFile.createNewFile();
+                Log.i("Diego", "GyroscopeFile Created");
+            } catch (Exception e) {
+                Log.e("Diego", "Could not create GyroscopeFile",e);
+            }
+        }
+
+        if(!MagnetometerFile.exists()){
+            try {
+                MagnetometerFile.createNewFile();
+                Log.i("Diego", "MagnetometerFile Created");
+            } catch (Exception e) {
+                Log.e("Diego", "Could not create MagnetometerFile",e);
+            }
+
+        }if(!TouchEventFile.exists()){
+            try {
+                TouchEventFile.createNewFile();
+                Log.i("Diego", "TouchEventFile Created");
+            } catch (Exception e) {
+                Log.e("Diego", "Could not create TouchEventFile",e);
+            }
+        }
+
+        final Button button = findViewById(R.id.btnEnd);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Diego", "Button Pressed");
+                BufferAccel = GravarArquivo(AccelerometerFile,BufferAccel,"", false);
+                BufferMag = GravarArquivo(MagnetometerFile,BufferMag,"", false);
+                BufferGyro = GravarArquivo(GyroscopeFile,BufferGyro,"", false);
+                BufferTouch = GravarArquivo(TouchEventFile, BufferTouch, "", false);
+                Intent intent = new Intent(getBaseContext(), EndActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        eventAction = event.getAction();
+        if (eventAction == MotionEvent.ACTION_DOWN || eventAction == MotionEvent.ACTION_UP ||
+                eventAction == MotionEvent.ACTION_POINTER_DOWN || eventAction == MotionEvent.ACTION_POINTER_UP ||
+                eventAction == MotionEvent.ACTION_MOVE){
+            unixTime = Long.toString(System.currentTimeMillis());
+            PosX = Float.toString(event.getX());
+            PosY = Float.toString(event.getY());
+            Press = Float.toString(event.getPressure());
+            Area = Float.toString(event.getSize());
+
+            //Escrever no Arquivo
+            BufferTouch = GravarArquivo(TouchEventFile, BufferTouch,unixTime + ',' + "123" + ',' + "123" + ','
+                    + event.getPointerCount() + ',' + '0' + ',' + eventAction + ',' + PosX + ',' + PosY + ','
+                    + Press + ',' + Area + ',' + PhoneOrientation + '\n', false);
+
+            Log.i("Diego", "Tocado");
+        }
+        return true;
+    }
+
+
+    protected void onResume(SensorEvent event) {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, 100000000);
+        mSensorManager.registerListener(this, mGyroscope, 100000000);
+        mSensorManager.registerListener(this, mMagnetometer, 100000000);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        SensorType = event.sensor.getType();
+        unixTime = Long.toString(System.currentTimeMillis());
+        display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        PhoneOrientation = display.getRotation();
+
+        if (SensorType == Sensor.TYPE_ACCELEROMETER){
+            AccelX = Float.toString(event.values[0]);
+            AccelY = Float.toString(event.values[1]);
+            AccelZ = Float.toString(event.values[2]);
+
+            //Gravacao dos Dados no Arquivo
+            BufferAccel = GravarArquivo(AccelerometerFile, BufferAccel,unixTime + ',' + "123" + ',' + "123" + ','
+                    + AccelX + ',' + AccelY + ',' + AccelZ + ','
+                    + PhoneOrientation + '\n', true);
+
+        }else if(SensorType == Sensor.TYPE_GYROSCOPE){
+            GyroX = Float.toString(event.values[0]);
+            GyroY = Float.toString(event.values[1]);
+            GyroZ = Float.toString(event.values[2]);
+
+            BufferGyro = GravarArquivo(GyroscopeFile, BufferGyro,unixTime + ',' + "123" + ',' + "123" + ','
+                    + GyroX + ',' + GyroY + ',' + GyroZ + ','
+                    + PhoneOrientation + '\n', true);
+
+
+        }else if(SensorType == Sensor.TYPE_MAGNETIC_FIELD){
+            MagX = Float.toString(event.values[0]);
+            MagY = Float.toString(event.values[1]);
+            MagZ = Float.toString(event.values[2]);
+
+            BufferMag = GravarArquivo(MagnetometerFile, BufferMag,unixTime + ',' + "123" + ',' + "123" + ','
+                    + MagX + ',' + MagY + ',' + MagZ + ','
+                    + PhoneOrientation + '\n', true);
+        }
+    }
+
+    @Override public void onBackPressed() {
+        if( mCollectorKeyboard.isCollectorKeyboardVisible() ) mCollectorKeyboard.hideCollectorKeyboard(); else this.finish();
+    }
+
+}
+
--- android/Aegis/app/src/main/java/pignus/aegis/MainActivity.java
+++ android/Aegis/app/src/main/java/pignus/aegis/MainActivity.java
@@ -6,19 +6,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_NAME = "USER_NAME";
+    EditText editText;
+    CollectorKeyboard mCollectorKeyboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        editText = findViewById(R.id.editTextName);
+        mCollectorKeyboard = new CollectorKeyboard(this, R.id.keyboardview2, R.xml.collector_keyboard, "mainActivity");
+        mCollectorKeyboard.registerEditText(R.id.editTextName);
         final Button button = findViewById(R.id.btnStart);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i("Diego", "Button Pressed");
-                Intent intent = new Intent(getBaseContext(), SensorsActivity.class);
+
+                Intent intent = new Intent(getBaseContext(), CollectorActivity.class);
+                intent.putExtra(USER_NAME, editText.getText().toString());
+
                 startActivity(intent);
             }
         });
